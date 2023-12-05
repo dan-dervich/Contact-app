ffcda8c219ff9c8105932500b0713b720a1e567e
dan-dervich <dandervich@hotmail.com>
2023-12-05 07:36:47 -0300

    fixed reply to in email config

--- ahavah.js
+++ ahavah.js
@@ -22,6 +22,7 @@ router.post("/send-mail", (req, res) => {
   let mailOptions = {
     from: process.env.AHAVAHEMAIL, // sender address
     to: "ventas@ahavahmoda.com", // list of receivers
+    replyTo: req.body.email,
     subject: "CONTACTO", // Subject line
     html: `<p>Nombre: ${req.body.name}</p><br/> <p>Email: ${req.body.email}</p><br/> <p>Telefono: ${req.body.tel}</p><br/> <p>Mensaje: ${req.body.msg}</p><br/>`, // html body
   };
