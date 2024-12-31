bbc3fbd990318fca500fc609e7f863d5cef0ef60
dan-dervich <dandervich@hotmail.com>
2024-12-31 19:31:52 -0300

    changed recipient email to sales@allforwater.com

--- afw.js
+++ afw.js
@@ -21,7 +21,7 @@ router.post("/send-mail", (req, res) => {
   // setup email data with unicode symbols
   let mailOptions = {
     from: process.env.AHAVAHEMAIL, // sender address
-    to: "dandervich@gmail.com", // list of receivers
+    to: "sales@allforwater.com", // list of receivers
     replyTo: req.body.email,
     subject: "CONTACTO", // Subject line
     html: `<p>Nombre: ${req.body.name}</p><br/> <p>Email: ${req.body.email}</p><br/> <p>Telefono: ${req.body.tel}</p><br/> <p>Mensaje: ${req.body.msg}</p><br/>`, // html body
